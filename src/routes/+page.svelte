93fe741861d2e948b6b838f48e613248982692f1
Koji <stylethewalker@gmail.com>
2023-06-06 14:28:19 -0300

    botões de exemplo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button type="button" class="btn">Base class</button>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" class="btn btn-link">Link</button>
+<h1 />
+<button type="button" class="btn btn-outline-primary">Primary</button>
+<button type="button" class="btn btn-outline-secondary">Secondary</button>
+<button type="button" class="btn btn-outline-success">Success</button>
+<button type="button" class="btn btn-outline-danger">Danger</button>
+<button type="button" class="btn btn-outline-warning">Warning</button>
+<button type="button" class="btn btn-outline-info">Info</button>
+<button type="button" class="btn btn-outline-light">Light</button>
+<button type="button" class="btn btn-outline-dark">Dark</button>
+<h1 />
+<a class="btn btn-primary" href="#" role="button">Link</a>
+<button class="btn btn-primary" type="submit">Button</button>
+<input class="btn btn-primary" type="button" value="Input" />
+<input class="btn btn-primary" type="submit" value="Submit" />
+<input class="btn btn-primary" type="reset" value="Reset" />
+<h1 />
+<div class="btn-group" role="group" aria-label="Basic example">
+	<button type="button" class="btn btn-primary">Left</button>
+	<button type="button" class="btn btn-primary">Middle</button>
+	<button type="button" class="btn btn-primary">Right</button>
+</div>
+<h1 />
+<div class="btn-group">
+	<a href="#" class="btn btn-primary active" aria-current="page">Active link</a>
+	<a href="#" class="btn btn-primary">Link</a>
+	<a href="#" class="btn btn-primary">Link</a>
+</div>
